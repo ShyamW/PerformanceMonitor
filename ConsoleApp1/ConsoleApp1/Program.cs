825c4c245fc1ea6b1e7d2a5c0b77607ba12b2c21
Shyamw <shyam.thiagarajan@gmail.com>
2017-06-20 23:17:03 -0500

    Initial Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            PerformanceCounterCategory[] perfCats = PerformanceCounterCategory.GetCategories();
+            foreach (PerformanceCounterCategory category in perfCats.OrderBy(c => c.CategoryName))
+            {
+                Console.WriteLine("Category Name: {0}", category.CategoryName);
+            }        
+            Console.ReadLine();
+        }
+    }
+}
